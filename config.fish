55feee10a875f8f5d97d30c3921d8983e2412321
Ryan Biazo <osmoke51@gmail.com>
2022-06-24 23:21:56 -0500

    First Attempt
    My first attempt at saving my config files somehow...

---
+++ config.fish
@@ -0,0 +1,20 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+function fish_prompt
+    echo (set_color FFF)'['(set_color blue)(whoami)(set_color FFF).(set_color cyan)(hostname) (set_color FFF)(dirs)'] '
+end
+
+set fish_greeting
+
+alias q='exit'
+alias e='exit'
+alias c='clear'
+alias reflect="sudo reflector --country 'United States',Canada --age 24 --protocol https --sort rate --save /etc/pacman.d/mirrorlist"
+alias logo="fish_logo"
+
+set fish_cursor_default block
+set fish_cursor_insert block
+set fish_cursor_replace_one block
+set fish_cursor_visual block
