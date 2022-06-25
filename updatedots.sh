2c2910d4039e27bab614b59cb3505b6d60734fcb
Ryan Biazo <osmoke51@gmail.com>
2022-06-25 02:19:55 -0500

    Update Script
    Created a script to "save" all the config files into this git project, updated readme and copied over proper polybar config.

---
+++ updatedots.sh
@@ -0,0 +1,8 @@
+cp /home/rbiazo/.config/fish/config.fish /home/rbiazo/GitHub/ConfigFiles/config.fish
+cp /home/rbiazo/.config/rofi/config.rasi /home/rbiazo/GitHub/ConfigFiles/config.rasi
+cp /home/rbiazo/.config/i3/config /home/rbiazo/GitHub/ConfigFiles/config_i3
+cp /home/rbiazo/.config/kitty/kitty.conf /home/rbiazo/GitHub/ConfigFiles/kitty.conf
+cp /home/rbiazo/.config/polybar/config.ini /home/rbiazo/GitHub/ConfigFiles/config.ini
+cp /etc/lightdm/lightdm.conf /home/rbiazo/GitHub/ConfigFiles/lightdm.conf
+cp /etc/lightdm/slick-greeter.conf /home/rbiazo/GitHub/ConfigFiles/slick-greeter.conf
+cp /etc/xdg/picom.conf /home/rbiazo/GitHub/ConfigFiles/picom.conf
