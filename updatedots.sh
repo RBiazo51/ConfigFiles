a70c1eacf49af1f922fa891be3afa784dd5859ef
Ryan Biazo <osmoke51@gmail.com>
2022-06-30 03:07:11 -0500

    Organizing
    Made folders for each program for better organization.

--- updatedots.sh
+++ updatedots.sh
@@ -1,11 +1,29 @@
-cp /home/rbiazo/.config/fish/config.fish /home/rbiazo/GitHub/ConfigFiles/config.fish
-cp /home/rbiazo/.config/rofi/config.rasi /home/rbiazo/GitHub/ConfigFiles/config.rasi
-cp /home/rbiazo/.config/i3/config /home/rbiazo/GitHub/ConfigFiles/config_i3
-cp /home/rbiazo/.config/kitty/kitty.conf /home/rbiazo/GitHub/ConfigFiles/kitty.conf
-cp /home/rbiazo/.config/polybar/config.ini /home/rbiazo/GitHub/ConfigFiles/config.ini
-cp /etc/lightdm/lightdm.conf /home/rbiazo/GitHub/ConfigFiles/lightdm.conf
-cp /etc/lightdm/slick-greeter.conf /home/rbiazo/GitHub/ConfigFiles/slick-greeter.conf
-cp /etc/xdg/picom.conf /home/rbiazo/GitHub/ConfigFiles/picom.conf
-cp /home/rbiazo/x /home/rbiazo/GitHub/ConfigFiles/x
-cp /home/rbiazo/mini /home/rbiazo/GitHub/ConfigFiles/mini
-cp /home/rbiazo/.local/bin/powermenu /home/rbiazo/GitHub/ConfigFiles/powermenu
+#fish
+mkdir /home/rbiazo/GitHub/ConfigFiles/fish
+cp -r /home/rbiazo/.config/fish/config.fish /home/rbiazo/GitHub/ConfigFiles/fish/config.fish
+
+#i3
+cp -r /home/rbiazo/.config/i3/ /home/rbiazo/GitHub/ConfigFiles/
+
+#kitty
+cp -r /home/rbiazo/.config/kitty/ /home/rbiazo/GitHub/ConfigFiles/
+
+#polybar
+cp -r /home/rbiazo/.config/polybar/ /home/rbiazo/GitHub/ConfigFiles/
+
+#rofi
+cp -r /home/rbiazo/.config/rofi/ /home/rbiazo/GitHub/ConfigFiles/
+
+#gotop
+mkdir /home/rbiazo/GitHub/ConfigFiles/gotop/
+cp /home/rbiazo/x /home/rbiazo/GitHub/ConfigFiles/gotop/x
+cp /home/rbiazo/mini /home/rbiazo/GitHub/ConfigFiles/gotop/mini
+
+#lightdm
+mkdir /home/rbiazo/GitHub/ConfigFiles/lightdm/
+cp /etc/lightdm/lightdm.conf /home/rbiazo/GitHub/ConfigFiles/lightdm/lightdm.conf
+cp /etc/lightdm/slick-greeter.conf /home/rbiazo/GitHub/ConfigFiles/lightdm/slick-greeter.conf
+
+#picom
+mkdir /home/rbiazo/GitHub/ConfigFiles/picom/
+cp /etc/xdg/picom.conf /home/rbiazo/GitHub/ConfigFiles/picom/picom.conf
