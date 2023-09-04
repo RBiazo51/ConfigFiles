fb85dab88594f708ada1748afc99135a7919316a
Ryan <osmoke51@gmail.com>
2023-09-03 23:05:17 -0500

    Update powermenu.sh

--- rofi/powermenu/powermenu.sh
+++ rofi/powermenu/powermenu.sh
@@ -5,7 +5,7 @@ uptime=$(uptime -p | sed -e 's/up //g')
 
 rofi_command="rofi -theme $dir/$theme"
 
-chosen="$(echo -e "Restart\nPower Off\nLogoff" | $rofi_command -p "Uptime: $uptime" -dmenu -selected-row 2)"
+chosen="$(echo -e "Logoff\nPower Off\nRestart" | $rofi_command -p "Uptime: $uptime" -dmenu -selected-row 1)"
 case $chosen in
 	"Power Off") poweroff ;;
 	"Restart") reboot ;;
