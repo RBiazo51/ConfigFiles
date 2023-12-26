b7052731cc438c91e37a0bf38a719aa4d06db1d9
Ryan <osmoke51@gmail.com>
2023-12-26 11:19:00 -0600

    Updated ToDo.sh

--- todo/ToDo.sh
+++ todo/ToDo.sh
@@ -1,3 +1,4 @@
 #Test pulldots.sh
 #Build and test pushdots.sh
 #Figure out how to make the peaclock config work
+#Rename .config/rofi/powermenu to something else since it's not just the powermenus now.
