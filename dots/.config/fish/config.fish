d587bf034989f34178ec3e8669ddb748bdf516cd
Ryan <osmoke51@gmail.com>
2023-12-25 22:03:59 -0600

    Updated 'whatismyip

--- dots/.config/fish/config.fish
+++ dots/.config/fish/config.fish
@@ -18,14 +18,23 @@ alias c='clear'
 #alias reflect="sudo nala fetch"
 alias tweather="curl 'v2.wttr.in/tontitown?u'"
 alias fweather="curl 'v2.wttr.in/fayetteville?u'"
-alias whatismyip="echo (set_color cyan)External(set_color reset);curl icanhazip.com;echo (set_color cyan)Internal(set_color reset);hostname -i"
 alias whatismydisk='echo (set_color cyan)Zeros(set_color reset);dd if=/dev/zero of=/tmp/test1.img bs=1G count=1 oflag=dsync;rm /tmp/test1.img;echo (set_color cyan)Random(set_color reset);dd if=/dev/random of=/tmp/test2.img bs=1G count=1 oflag=dsync;rm /tmp/test2.img'
 alias whatismyspeed='echo (set_color cyan)Testing Internet Speed Now(set_color reset);speedtest --secure | grep Mbit/s'
+
 function whatismycolor
     for i in BLACK brBLACK BLUE brBLUE GREEN brGREEN YELLOW brYELLOW CYAN brCYAN WHITE brWHITE MAGENTA brMAGENTA RED brRED NORMAL
         echo (set_color $i)$i
     end
 end
+
+function whatismyip                                                                        
+	echo (set_color cyan)External(set_color normal)                                        
+	curl icanhazip.com                                                                     
+	echo (set_color cyan)Internal(set_color normal)                                        
+	ip addr show | awk '/inet / && !/127.0.0.1/ {                                          
+		printf "\033[1;31m%s\033[0m\n%s\n", toupper($NF), $2                               
+	}' | cut -d"/" -f1                                                                     
+end 
 
 set fish_cursor_default block
 set fish_cursor_insert block
