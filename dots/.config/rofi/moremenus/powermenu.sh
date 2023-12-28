d9a23ca81f4ebf7d95210cea41b46751e9e17570
Ryan <osmoke51@gmail.com>
2023-12-27 20:08:12 -0600

    Updated 'powermenu' for Rofi

--- dots/.config/rofi/moremenus/powermenu.sh
+++ dots/.config/rofi/moremenus/powermenu.sh
@@ -1,4 +1,3 @@
-#This is still WIP to get it working on both Debian and Arch
 theme="card_square"
 dir="$HOME/.config/rofi/moremenus"
 
@@ -8,8 +7,8 @@ rofi_command="rofi -theme $dir/$theme"
 
 chosen="$(echo "Logoff\nPower Off\nRestart" | $rofi_command -p "Uptime: $uptime" -dmenu -selected-row 1)"
 case $chosen in
-	"Power Off") kitty sudo shutdown now ;;
-	"Restart") kitty sudo reboot now ;;
+	"Power Off") systemctl poweroff ;;
+	"Restart") systemctl reboot ;;
 	"Logoff") i3-msg exit ;;
 	*) exit 1 ;;
 esac
