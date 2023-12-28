a413bd366afc45579439f7327fb56b4a0821fcae
Ryan <osmoke51@gmail.com>
2023-12-27 19:52:52 -0600

    Updated 'ipmenu2' for Rofi

--- dots/.config/rofi/moremenus/ipmenu2.sh
+++ dots/.config/rofi/moremenus/ipmenu2.sh
@@ -4,22 +4,13 @@ dir="$HOME/.config/rofi/moremenus"
 uptime=$(uptime -p | sed -e 's/up //g')
 
 external=$(curl -s icanhazip.com)
-internal=$(ip addr | awk '/inet / && $2 !~ /^127\./ {print $2}' | cut -f1 -d'/')
+internal=$(ip addr | awk '/inet / && $2 !~ /^127\./ {gsub(/\/.*/, "", $2); print $2}')
 
 rofi_command="rofi -theme $dir/$theme"
 
-ip_info=""
-for interface in $internal; do
-    ip=$(ip addr show dev $interface | awk '/inet /{print $2}' | cut -f1 -d'/')
-    ip_info+="\n$interface: $ip"
-done
-
-chosen="$(echo -e "$external$ip_info" | $rofi_command -p "IP Menu" -dmenu -selected-row 1)"
+chosen="$(echo -e "$external\n$internal" | $rofi_command -p "IP Menu" -dmenu -selected-row 1)"
 case $chosen in
     "$external") echo -n "$external" | xclip -selection clipboard;;
-    *) # Extract the selected interface and its IP
-       selected_interface=$(echo "$chosen" | awk -F ": " '{print $1}')
-       selected_ip=$(echo "$chosen" | awk -F ": " '{print $2}' | awk '{gsub(/:/, ""); print}')
-       echo -n "$selected_ip" | xclip -selection clipboard
-       ;;
+    "$internal") echo -n "$internal" | xclip -selection clipboard;;
+    *) exit 1 ;;
 esac
