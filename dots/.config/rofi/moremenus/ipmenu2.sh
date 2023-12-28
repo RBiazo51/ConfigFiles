d1a10ddd66f0dd929fb2998c7f4cbb81bcf68315
Ryan <osmoke51@gmail.com>
2023-12-27 20:03:20 -0600

    Updated 'ipmenu2' for Rofi

--- dots/.config/rofi/moremenus/ipmenu2.sh
+++ dots/.config/rofi/moremenus/ipmenu2.sh
@@ -8,17 +8,11 @@ internal=$(ip addr | awk '/inet / && $2 !~ /^127\./ {gsub(/\/.*/, "", $2); print
 
 rofi_command="rofi -theme $dir/$theme"
 
-ip_info=""
-for interface in $internal; do
-    ip=$(ip addr show dev $interface | awk '/inet /{gsub(/\/.*/, "", $2); print $2}')
-    ip_info+="\n$interface: $ip"
-done
-
-chosen="$(echo -e "External: $external$ip_info" | $rofi_command -p "IP Menu" -dmenu -selected-row 1)"
+chosen="$(echo -e "$external\n$internal" | $rofi_command -p "IP Menu" -dmenu -selected-row 1)"
 case $chosen in
-    "External:"*) echo -n "$external" | xclip -selection clipboard;;
+    "$external") echo -n "$external" | xclip -selection clipboard;;
     *) # Extract the selected IP
-       selected_ip=$(echo "$chosen" | awk '{print $2}')
+       selected_ip=$(echo "$chosen" | awk '{print $1}')
        echo -n "$selected_ip" | xclip -selection clipboard
        ;;
 esac
