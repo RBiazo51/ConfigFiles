b59b2f9fe7b3d9ed0674e888f4dac611b5214e6b
Ryan <osmoke51@gmail.com>
2023-12-27 20:04:10 -0600

    Updated 'ipmenu' for Rofi

--- dots/.config/rofi/moremenus/ipmenu.sh
+++ dots/.config/rofi/moremenus/ipmenu.sh
@@ -1,23 +1,18 @@
-#This is mostly working, but needs to be teseted on a computer with multiple interfaces.
-#I would like it to show the interface names and IP like below, and allow you to select between them
-
-#WLAN0: 192.168.0.1
-#External: x.x.x.x
-#WLP3S0: 10.55.55.1
-
 theme="card_square"
 dir="$HOME/.config/rofi/moremenus"
 
 uptime=$(uptime -p | sed -e 's/up //g')
 
-external=$(curl icanhazip.com)
-internal=$(hostname -I | cut -f1 -d' ')
+external=$(curl -s icanhazip.com)
+internal=$(ip addr | awk '/inet / && $2 !~ /^127\./ {gsub(/\/.*/, "", $2); print $2}')
 
 rofi_command="rofi -theme $dir/$theme"
 
-chosen="$(echo "$external\n$internal" | $rofi_command -p "IP Menu" -dmenu -selected-row 1)"
+chosen="$(echo -e "$external\n$internal" | $rofi_command -p "IP Menu" -dmenu -selected-row 1)"
 case $chosen in
-	"$external") echo -n "$external" | xclip -selection clipboard;;
-    "$internal") echo -n "$internal" | xclip -selection clipboard;;
-	*) exit 1 ;;
+    "$external") echo -n "$external" | xclip -selection clipboard;;
+    *) # Extract the selected IP
+       selected_ip=$(echo "$chosen" | awk '{print $1}')
+       echo -n "$selected_ip" | xclip -selection clipboard
+       ;;
 esac
