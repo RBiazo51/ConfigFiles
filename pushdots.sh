3989d1c5a3594fd11e9994e7291f59907b78c1fc
Ryan Biazo <osmoke51@gmail.com>
2023-12-31 16:58:43 -0600

    Updated 'pushdots' Comments

--- pushdots.sh
+++ pushdots.sh
@@ -1,10 +1,10 @@
 # NEEDS TESTING
-# copy entire i3 kitty polybar rofi folder from .config
+# copy entire i3 kitty polybar rofi folder from ~/.config
 for i in kitty polybar rofi i3
     cp -r ~/.config/$i/* ./dots/$i/
 end
 
-# Copy only config.fish from .config
+# Copy only config.fish from ~/.config
 cp ~/.config/fish/config.fish ./dots/fish/config.fish
 
 # copy only picom.conf from /etc/xdg/
