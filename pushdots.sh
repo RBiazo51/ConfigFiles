f20b27990ceea1438f04f1dd7d3167f8299def9c
Ryan Biazo <osmoke51@gmail.com>
2023-12-31 16:55:34 -0600

    Organize and Modify 'pushdots'

--- pushdots.sh
+++ pushdots.sh
@@ -1,13 +1,15 @@
-#NEEDS TESTING
-#copy fish i3 kitty polybar rofi from .config NOT THE WHOLE FOLDER
+# NEEDS TESTING
+# copy entire i3 kitty polybar rofi folder from .config
 for i in kitty polybar rofi i3
-    cp -r ~/.config/$i/* ./dots/.config/$i/
+    cp -r ~/.config/$i/* ./dots/$i/
 end
-cp ~/.config/fish/config.fish ./dots/.config/fish/config.fish
 
-#copy picom from /etc/ NOT THE WHOLE FOLDER
-cp /etc/xdg/picom.conf ./dots/etc/picom/picom.conf
+# Copy only config.fish from .config
+cp ~/.config/fish/config.fish ./dots/fish/config.fish
 
-#copy .mini .x from ~/
+# copy only picom.conf from /etc/xdg/
+cp /etc/xdg/picom.conf ./dots/picom/picom.conf
+
+# copy .mini .x from ~/
 cp ~/.mini ./dots/gotop/.mini
 cp ~/.x ./dots/gotop/.x
