580f82507501145f7176faffbfa6e6cf658ba8db
Ryan Biazo <osmoke51@gmail.com>
2023-12-31 14:49:39 -0600

    Fixed 'pushdots'

--- pushdots.sh
+++ pushdots.sh
@@ -1,8 +1,13 @@
 #NEEDS TESTING
 #copy fish i3 kitty polybar rofi from .config NOT THE WHOLE FOLDER
+for i in kitty polybar rofi i3
+    cp -r ~/.config/$i/* ./dots/.config/$i/
+end
+cp ~/.config/fish/config.fish ./dots/.config/fish/config.fish
 
-#copy lightdm picom from /etc/ NOT THE WHOLE FOLDER
+#copy picom from /etc/ NOT THE WHOLE FOLDER
+cp /etc/xdg/picom.conf ./dots/etc/picom/picom.conf
 
 #copy .mini .x from ~/
-cp ~/.mini ./.mini
-cp ~/.x ./.x
+cp ~/.mini ./dots/gotop/.mini
+cp ~/.x ./dots/gotop/.x
