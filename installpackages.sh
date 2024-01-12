76906287cce0851b07a96111234c9d7b49d667d5
Ryan <osmoke51@gmail.com>
2024-01-12 17:48:58 -0600

    Updated installpackages.sh

--- installpackages.sh
+++ installpackages.sh
@@ -1,6 +1,9 @@
+mkdir ~/GitHub
+cd ~/GitHub
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
+cd ~/GitHub
 
 yay -S kitty fish polybar picom rofi rofi-calc rofi-greenclip firefox pywal-git pfetch gotop peaclock inetutils nemo brightnessctl gparted feh ncdu man puzzles speedtest tree imagemagick arandr 7-zip reflector speedtest-cli
 
