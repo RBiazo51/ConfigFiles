ef32b6e43ee6f60b25aee746e04185ff583d24c3
Ryan <osmoke51@gmail.com>
2024-01-12 15:39:10 -0600

    Created installpackages.sh

---
+++ installpackages.sh
@@ -0,0 +1,5 @@
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+yay -S polybar rofi rofi-calc rofi-greenclip firefox pywal-git pfetch gotop peaclock inetutils nemo brightnessctl gparted fish feh ncdu man puzzles speedtest tree imagemagick arandr 7-zip
