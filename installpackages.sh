d87b60a7815c89b43004923dcdd8b2da7a286de2
Ryan <osmoke51@gmail.com>
2024-01-12 17:44:40 -0600

    Updated installpackages.sh

--- installpackages.sh
+++ installpackages.sh
@@ -2,4 +2,7 @@ git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 
-yay -S kitty fish polybar picom rofi rofi-calc rofi-greenclip firefox pywal-git pfetch gotop peaclock inetutils nemo brightnessctl gparted feh ncdu man puzzles speedtest tree imagemagick arandr 7-zip
+yay -S kitty fish polybar picom rofi rofi-calc rofi-greenclip firefox pywal-git pfetch gotop peaclock inetutils nemo brightnessctl gparted feh ncdu man puzzles speedtest tree imagemagick arandr 7-zip reflector
+
+git clone https://github.com/rbiazo51/Backgrounds
+wal -i ~/GitHub/Backgrounds/eve1.png --saturate .6
