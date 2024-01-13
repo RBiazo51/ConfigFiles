3f71844599462348044c4e1463366d37fc41943a
Ryan <osmoke51@gmail.com>
2024-01-12 18:36:17 -0600

    Updated installpackages.sh

--- installpackages.sh
+++ installpackages.sh
@@ -8,6 +8,8 @@ cd ~/GitHub
 yay -S reflector
 sudo reflector --country 'United States',Canada --age 12 --protocol https --sort rate --save /etc/pacman.d/mirrorlist --verbose --latest 50 --number 15
 
+sudo cp ~/GitHub/ConfigFiles/fonts/FragmentMono/FragmentMono-Regular.ttf /usr/share/fonts/FragmentMono.ttf
+
 yay -S 7-zip arandr brightnessctl feh firefox fish git gotop gparted grub-customizer gtk-chtheme htop imagemagick inetutils kitty lightdm-settings lxappearance man matcha-gtk-theme micro ncdu nemo peaclock pfetch picom polybar puzzles pywal-git rofi rofi-calc rofi-greenclip speedtest-cli sudo tree
 
 git clone https://github.com/rbiazo51/Backgrounds
