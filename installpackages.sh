68e542bb52a6a23670afc6fb8a7001e0f083577e
Ryan <osmoke51@gmail.com>
2024-01-12 18:26:31 -0600

    Updated installpackages.sh

--- installpackages.sh
+++ installpackages.sh
@@ -5,7 +5,10 @@ cd yay
 makepkg -si
 cd ~/GitHub
 
-yay -S 7-zip arandr brightnessctl feh firefox fish git gotop gparted grub-customizer gtk-chtheme htop imagemagick inetutils kitty lightdm-settings lxappearance man matcha-gtk-theme micro ncdu nemo peaclock pfetch picom polybar puzzles pywal-git reflector rofi rofi-calc rofi-greenclip speedtest-cli sudo tree
+yay -S reflector
+sudo reflector --country 'United States',Canada --age 12 --protocol https --sort rate --save /etc/pacman.d/mirrorlist --verbose --latest 50 --number 15
+
+yay -S 7-zip arandr brightnessctl feh firefox fish git gotop gparted grub-customizer gtk-chtheme htop imagemagick inetutils kitty lightdm-settings lxappearance man matcha-gtk-theme micro ncdu nemo peaclock pfetch picom polybar puzzles pywal-git rofi rofi-calc rofi-greenclip speedtest-cli sudo tree
 
 git clone https://github.com/rbiazo51/Backgrounds
 wal -i ~/GitHub/Backgrounds/eve1.png --saturate .6
