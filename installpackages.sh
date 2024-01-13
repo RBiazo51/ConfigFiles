72bb626b1d374f37f21b9ebf476a8736c00a98a4
Ryan <osmoke51@gmail.com>
2024-01-12 18:12:27 -0600

    Updated installpackages.sh

--- installpackages.sh
+++ installpackages.sh
@@ -5,7 +5,7 @@ cd yay
 makepkg -si
 cd ~/GitHub
 
-yay -S kitty fish polybar picom rofi rofi-calc rofi-greenclip firefox pywal-git pfetch gotop peaclock inetutils nemo brightnessctl gparted feh ncdu man puzzles speedtest tree imagemagick arandr 7-zip reflector speedtest-cli
+yay -S 7-zip arandr brightnessctl feh firefox fish gotop gparted grub-customizer gtk-chtheme imagemagick inetutils kitty lightdm-settings lxappearance man matcha-gtk-theme ncdu nemo peaclock pfetch picom polybar puzzles pywal-git reflector rofi rofi-calc rofi-greenclip speedtest speedtest-cli tree
 
 git clone https://github.com/rbiazo51/Backgrounds
 wal -i ~/GitHub/Backgrounds/eve1.png --saturate .6
