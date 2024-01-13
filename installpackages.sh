1804494fa0755eda99415f5831ee28d791595f75
Ryan <osmoke51@gmail.com>
2024-01-12 18:41:34 -0600

    Updated installpackages.sh

--- installpackages.sh
+++ installpackages.sh
@@ -5,12 +5,14 @@ cd yay
 makepkg -si
 cd ~/GitHub
 
-yay -S reflector
+yay -S reflector --noconfirm
 sudo reflector --country 'United States',Canada --age 12 --protocol https --sort rate --save /etc/pacman.d/mirrorlist --verbose --latest 50 --number 15
 
 sudo cp ~/GitHub/ConfigFiles/fonts/FragmentMono/FragmentMono-Regular.ttf /usr/share/fonts/FragmentMono.ttf
 
-yay -S 7-zip arandr brightnessctl feh firefox fish git gotop gparted grub-customizer gtk-chtheme htop imagemagick inetutils kitty lightdm-settings lxappearance man matcha-gtk-theme micro ncdu nemo peaclock pfetch picom polybar puzzles pywal-git rofi rofi-calc rofi-greenclip speedtest-cli sudo tree
+yay -S 7-zip arandr brightnessctl feh firefox fish git gotop gparted grub-customizer gtk-chtheme htop imagemagick inetutils kitty lightdm-settings lxappearance man matcha-gtk-theme micro ncdu nemo peaclock pfetch picom polybar puzzles pywal-git rofi rofi-calc rofi-greenclip speedtest-cli sudo tree --noconfirm
 
 git clone https://github.com/rbiazo51/Backgrounds
 wal -i ~/GitHub/Backgrounds/eve1.png --saturate .6
+
+fish ~/GitHub/ConfigFiles/pulldots.sh
