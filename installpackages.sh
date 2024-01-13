6895247d1ac8145e8fbc8177d89a61c4b3e56546
Ryan <osmoke51@gmail.com>
2024-01-12 20:10:41 -0600

    Updated installpackages.sh

--- installpackages.sh
+++ installpackages.sh
@@ -10,7 +10,7 @@ sudo reflector --country 'United States',Canada --age 12 --protocol https --sort
 
 sudo cp ~/GitHub/ConfigFiles/fonts/FragmentMono/FragmentMono-Regular.ttf /usr/share/fonts/FragmentMono.ttf
 
-yay -S 7-zip arandr brightnessctl feh firefox fish git gotop gparted grub-customizer gtk-chtheme htop imagemagick inetutils kitty lightdm-settings lxappearance man matcha-gtk-theme micro ncdu nemo numlockx peaclock pfetch picom polybar puzzles pywal-git rofi rofi-calc rofi-greenclip speedtest-cli sudo tree --noconfirm
+yay -S 7-zip arandr brightnessctl feh firefox fish git gotop gparted grub-customizer gtk-chtheme htop imagemagick inetutils kitty lightdm-settings lxappearance man matcha-gtk-theme micro mousepad ncdu nemo numlockx peaclock pfetch picom polybar puzzles pywal-git rofi rofi-calc rofi-greenclip speedtest-cli sudo tree --noconfirm
 
 git clone https://github.com/rbiazo51/Backgrounds
 wal -i ~/GitHub/Backgrounds/eve1.png --saturate .6
